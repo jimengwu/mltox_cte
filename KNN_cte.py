b496ae1c5a9501758b95d6d58b3db897fc8d20f1
wujim <wjmengmeng@outlook.com>
2023-04-13 11:14:50 +0200

    srasar & knn

--- KNN_cte.py
+++ KNN_cte.py
@@ -1,4 +1,5 @@
-from helper_model import *
+# from helper_model import *
+from helper_cte_model import *
 import numpy as np
 from time import ctime
 import argparse
@@ -10,9 +11,7 @@ def getArguments():
     parser = argparse.ArgumentParser(description="Running KNN_model for datasets.")
     parser.add_argument("-i", "--input", dest="inputFile", required=True)
     parser.add_argument("-e", "--encoding", dest="encoding", default="binary")
-    parser.add_argument(
-        "-l", "--leaf_ls", dest="leaf_list", required=True, nargs="+", type=int
-    )
+    parser.add_argument("-l", "--leaf_ls", dest="leaf_list", required=True, nargs="+")
     parser.add_argument(
         "-n", "--neighbors", dest="neighbors", required=True, nargs="+", type=int
     )
@@ -21,7 +20,7 @@ def getArguments():
 
 
 rand = random.randrange(1, 100)
-
+invitro = True  # set whether the in vitro information is included as input feature
 categorical = [
     "class",
     "tax_order",
@@ -36,10 +35,10 @@ categorical = [
     "obs_duration_mean",
     "invitro_label",
 ]
-invitro = True
+
 if invitro:
     categorical = ["class", "tax_order", "family", "genus", "species"]
-# non_categorical was numerical features, whcih will be standarized. \
+# non_categorical was numerical features, which will be standarized. \
 # Mol,bonds_number, atom_number was previously log transformed due to the maginitude of their values.
 
 non_categorical = [
@@ -66,7 +65,7 @@ elif args.encoding == "multiclass":
     encoding = "multiclass"
     encoding_value = [0.1, 1, 10, 100]
 
-# loading data & splitting into train and test dataset
+# -----------loading data & splitting into train and test dataset--------
 print("loading dataset...", ctime())
 db_mortality = load_data(
     args.inputFile,
@@ -76,39 +75,31 @@ db_mortality = load_data(
     seed=42,
 )
 print("finish loaded.", ctime())
-# db_mortality = db_mortality[:300]
 
+# -----------------startified split the dataset for training model--------
 df_fishchem = db_mortality[["fish", "test_cas"]]
 test_size = 0.2
 col_groups = "test_cas"
 
-trainvalid_idx, test_idx = get_grouped_train_test_split(
+traintest_idx, valid_idx = get_grouped_train_test_split(
     df_fishchem, test_size, col_groups
 )
-df_fishchem_tv = df_fishchem.iloc[trainvalid_idx, :].reset_index(drop=True)
-X_train = (
+df_fishchem_tv = df_fishchem.iloc[traintest_idx, :].reset_index(drop=True)
+
+X_traintest = (
     db_mortality.drop(columns="conc1_mean")
-    .iloc[trainvalid_idx, :]
+    .iloc[traintest_idx, :]
     .reset_index(drop=True)
 )
-if "conc1_mean" in list(X_train.columns):
-    print("yes")
-else:
-    print("no")
-
-X_test = (
-    db_mortality.drop(columns="conc1_mean").iloc[test_idx, :].reset_index(drop=True)
+X_valid = (
+    db_mortality.drop(columns="conc1_mean").iloc[valid_idx, :].reset_index(drop=True)
 )
 
-Y_train = db_mortality.iloc[trainvalid_idx, :].conc1_mean
-Y_test = db_mortality.iloc[test_idx, :].conc1_mean
+Y_traintest = db_mortality.iloc[traintest_idx, :].conc1_mean
+Y_valid = db_mortality.iloc[valid_idx, :].conc1_mean
 
 
-# X_train, X_test, Y_train, Y_test = train_test_split(
-#     X, Y, test_size=0.2, random_state=42
-# )
-
-# using 5-fold cross validation to choose the alphas with best accuracy
+# ------using 5-fold cross validation to choose the alphas with best accuracy-----
 
 # sequence_alpha = np.concatenate([
 #     np.logspace(-1.5, -1, 20),
@@ -118,35 +109,15 @@ Y_test = db_mortality.iloc[test_idx, :].conc1_mean
 # np.logspace(-0.1, 1, 30),
 # np.logspace(-0.8, 0, 10)
 # ])
-
-# sequence_alpha = [0.06723357536499334, 3.039195382313198]
-# [0.5623, 0.0749, 0.2212, 1.7433, 0.0833, 0.9459]
-# sequence_alpha = [0.01268961003167922,0.3562247890262442, 0.08531678524172806, 4.893900918477494, 0.06723357536499334, 3.039195382313198]
-# np.concatenate(
-# [np.logspace(-3, 0, 25), np.linspace(1.5, 10, 5)])
-sequence_alpha = np.logspace(-5, 0, 30)
+# sequence_alpha = np.logspace(-5, 0, 30)
 sequence_alpha = np.logspace(-2, 1, 30)
-# sequence_alpha = np.concatenate(
-#     [
-#         np.logspace(-5, 0, 30),
-#         np.array(
-#             [
-#                 0.01268961003167922,
-#                 0.3562247890262442,
-#                 0.08531678524172806,
-#                 4.893900918477494,
-#                 0.06723357536499334,
-#                 3.039195382313198,
-#             ]
-#         ),
-#     ]
-# )
+sequence_alpha = np.logspace(-2, 1, 3)  # just for testing
 
-best_alpha_h, best_alpha_p, best_leaf, best_neighbor, best_results = select_alpha(
+best_results = select_alpha(
     df_fishchem_tv,
     col_groups,
-    X_train,
-    Y_train,
+    X_traintest,
+    Y_traintest,
     sequence_alpha,
     categorical,
     non_categorical,
@@ -154,119 +125,65 @@ best_alpha_h, best_alpha_p, best_leaf, best_neighbor, best_results = select_alph
     args.neighbors,
     encoding,
 )
-print(ctime())
+print("training finished.", ctime())
 
-# validate on the test dataset
-minmax = MinMaxScaler().fit(X_train[non_categorical])
-X_train[non_categorical] = minmax.transform(X_train.loc[:, non_categorical])
-X_test[non_categorical] = minmax.transform(X_test.loc[:, non_categorical])
+# ----------------------validate on dataset---------------
+minmax = MinMaxScaler().fit(
+    X_traintest[non_categorical]
+)  # normalized the validation dataset
+X_traintest[non_categorical] = minmax.transform(X_traintest.loc[:, non_categorical])
+X_valid[non_categorical] = minmax.transform(X_valid.loc[:, non_categorical])
 
-matrix = dist_matrix(
-    X_test, X_train, non_categorical, categorical, best_alpha_h, best_alpha_p
+
+matrix_traintest = dist_matrix(
+    X_traintest,
+    X_traintest,
+    non_categorical,
+    categorical,
+    best_results.iloc[0]["ah"],
+    best_results.iloc[0]["ap"],
 )
-matrix_train = dist_matrix(
-    X_train, X_train, non_categorical, categorical, best_alpha_h, best_alpha_p
+matrix_valid = dist_matrix(
+    X_valid,
+    X_traintest,
+    non_categorical,
+    categorical,
+    best_results.iloc[0]["ah"],
+    best_results.iloc[0]["ap"],
 )
+
 neigh = KNeighborsClassifier(
-    n_neighbors=best_neighbor, metric="precomputed", leaf_size=best_leaf
-)
-neigh.fit(matrix_train, Y_train.astype("int").ravel())
-y_pred = neigh.predict(matrix)
-
-if encoding == "binary":
-
-    accs = accuracy_score(Y_test, y_pred)
-    sens = recall_score(Y_test, y_pred, average="macro")
-    tn, fp, fn, tp = confusion_matrix(Y_test, y_pred, labels=[0, 1]).ravel()
-    specs = tn / (tn + fp)
-    precs = precision_score(Y_test, y_pred, average="macro")
-    f1 = f1_score(Y_test, y_pred, average="macro")
-
-elif encoding == "multiclass":
-    accs = accuracy_score(Y_test, y_pred)
-    sens = recall_score(Y_test, y_pred, average="macro")
-    specs = np.nan
-    precs = precision_score(Y_test, y_pred, average="macro")
-    f1 = f1_score(Y_test, y_pred, average="macro")
-
-
-print(
-    "Accuracy: ",
-    accs,
-    "se:",
-    best_results["se_accs"],
-    "\n Sensitivity:",
-    sens,
-    "se:",
-    best_results["se_sens"],
-    "\n Specificity",
-    specs,
-    "se:",
-    best_results["se_specs"],
-    "\n Precision",
-    precs,
-    "se:",
-    best_results["se_precs"],
-    "\n F1 score:",
-    f1,
-    "se:",
-    best_results["se_f1"],
+    n_neighbors=best_results.iloc[0]["neighbor"],
+    metric="precomputed",
+    leaf_size=best_results.iloc[0]["leaf"],
 )
 
-# saving the information into a file
-info = []
-info.append(
-    """The best params were alpha_h:{}, alpha_p:{} ,leaf:{},neighbor:{}""".format(
-        best_alpha_h, best_alpha_p, best_leaf, best_neighbor
-    )
-)
-info.append(
-    """Accuracy:  {}, Se.Accuracy:  {} 
-		\nSensitivity:  {}, Se.Sensitivity: {}
-        \nSpecificity:  {}, Se.Specificity:{}
-		\nPrecision:  {}, Se.Precision: {}
-		\nf1_score:{}, Se.f1_score:{}""".format(
-        accs,
-        best_results["se_accs"],
-        sens,
-        best_results["se_sens"],
-        specs,
-        best_results["se_specs"],
-        precs,
-        best_results["se_precs"],
-        f1,
-        best_results["se_f1"],
-    )
+df_output = fit_and_predict(
+    neigh,
+    matrix_traintest,
+    Y_traintest.astype("int").ravel(),
+    matrix_valid,
+    Y_valid,
+    encoding,
 )
 
-# info.append('The parameters was selected from {}'.format(
-#     'np.concatenate([np.logspace(-1.5,-1,20),np.logspace(-3,0,25), np.linspace(1.5,10,5), \
-#     np.logspace(-2,1,30), np.logspace(-0.1, 1, 30), np.logspace(-0.8, 0,10)])')
-#             )
+# --------saving the information into a file
 
-# info.append('The parameters was selected from {}'.format(
-#     '[0.5623, 0.0749, 0.2212, 1.7433, 0.0833, 0.9459]')
-# )
-# info.append('The parameters was selected from {}'.format(
-#     'np.logspace(-2, 1, 30)')
-#             )
+df_output.loc[0, ["ah", "ap", "leaf", "neighbor"]] = best_results.iloc[0][
+    ["ah", "ap", "leaf", "neighbor"]
+]
+df_output.loc[0, "sequence_alpha"] = str(sequence_alpha)
+df_output.loc[0, "leaf_list"] = str(args.leaf_list)
+df_output.loc[0, "encoding"] = args.encoding
 
-info.append("The parameters was selected from {}".format("np.logspace(-2, 0, 30)"))
-info.append("The leaf was selected from {}".format(args.leaf_list))
-info.append("Random state:{}".format(rand))
+result = pd.concat([df_output, best_results], keys=["valid", "train"])
 
-filename = args.outputFile
-dirname = os.path.dirname(filename)
-if not os.path.exists(dirname):
-    os.makedirs(dirname)
-with open(filename, "w") as file_handler:
-    for item in info:
-        file_handler.write("{}\n".format(item))
+df2file(result, args.outputFile)
 
 
-# python KNN_cte.py -i  /local/wujimeng/code_jimeng/data/invivo/lc50_processed_jim.csv   -l 10 -n 1 -o   knn/1nn_bi2.txt
-# python KNN_cte.py -i  /local/wujimeng/code_jimeng/data/invivo/lc50_processed_jim.csv   -l 10 -n 3 -o   knn/3nn_bi2.txt
-# python KNN_cte.py -i  /local/wujimeng/code_jimeng/data/invivo/lc50_processed_jim.csv   -l 10 -n 5 -o   knn/5nn_bi2.txt
+# python KNN_cte.py -i  /local/wujimeng/code_jimeng/data/invivo/lc50_processed_jim.csv   -l 10 -n 1 -o   knn/1nn_bi.txt
+# python KNN_cte.py -i  /local/wujimeng/code_jimeng/data/invivo/lc50_processed_jim.csv   -l 10 -n 3 -o   knn/3nn_bi.txt
+# python KNN_cte.py -i  /local/wujimeng/code_jimeng/data/invivo/lc50_processed_jim.csv   -l 10 -n 5 -o   knn/5nn_bi.txt
 
 
 # python KNN_cte.py -i /local/wujimeng/code_jimeng/data/invivo/lc50_processed_jim.csv  -e "multiclass" -l 10 30 50 70 90  -n 1 -o   knn/1nn_mul.txt
--- RASAR_simple_cte.py
+++ RASAR_simple_cte.py
@@ -1,30 +1,42 @@
-from helper_model_cte import *
-from scipy.spatial.distance import cdist, pdist, squareform
-from collections import Counter
-from sklearn.model_selection import train_test_split, ParameterSampler
-import h2o
+from helper_cte_model import *
+from sklearn.model_selection import ParameterSampler
 from sklearn.ensemble import RandomForestClassifier
-from tqdm import tqdm
 import argparse
-import sys
 import os
 
 
 def getArguments():
     parser = argparse.ArgumentParser(description="Running KNN_model for datasets.")
-    parser.add_argument("-i1", "--input", dest="inputFile", required=True)
-    parser.add_argument("-wif", "--invitroFile", dest="invitroFile", default="False")
+    parser.add_argument("-i", "--input", dest="inputFile", required=True)
+    parser.add_argument("-ah", "--alpha_h", dest="alpha_h", required=True, nargs="?")
+    parser.add_argument("-ap", "--alpha_p", dest="alpha_p", required=True, nargs="?")
+    parser.add_argument("-dbi", "--db_invitro", dest="db_invitro", default="noinvitro")
     parser.add_argument("-e", "--encoding", dest="encoding", default="binary")
+    parser.add_argument(
+        "-if",
+        "--invitroFile",
+        dest="invitroFile",
+        help="if input is invitroFile",
+        default=False,
+    )
     parser.add_argument(
         "-il", "--invitro_label", dest="invitro_label", default="number"
     )
-    parser.add_argument("-dbi", "--db_invitro", dest="db_invitro", default="noinvitro")
-    parser.add_argument("-wi", "--w_invitro", dest="w_invitro", default="False")
-    parser.add_argument("-ah", "--alpha_h", dest="alpha_h", required=True, nargs="?")
-    parser.add_argument("-ap", "--alpha_p", dest="alpha_p", required=True, nargs="?")
+    parser.add_argument(
+        "-m", "--model", help="model: logistic regression, random forest", default="rf",
+    )
     parser.add_argument(
         "-n", "--n_neighbors", dest="n_neighbors", nargs="?", default=1, type=int
     )
+    parser.add_argument(
+        "-ni",
+        "--niter",
+        help="number of iterations to find the best parameter combination",
+        nargs="?",
+        default=100,
+        type=int,
+    )
+    parser.add_argument("-wi", "--w_invitro", dest="w_invitro", default="False")
     parser.add_argument("-o", "--output", dest="outputFile", default="binary.txt")
     return parser.parse_args()
 
@@ -44,7 +56,10 @@ if args.invitroFile == "True":
 db_invitro_matrix = None
 
 rand = random.randrange(1, 100)
+
 conc_column = "conc1_mean"
+
+# -----------loading data & splitting into train and test dataset--------
 db_mortality = load_data(
     args.inputFile,
     encoding=encoding,
@@ -54,41 +69,36 @@ db_mortality = load_data(
     seed=rand,
 )
 print("finish loaded.", ctime())
-# db_mortality = db_mortality[:300]
 
+# -----------------startified split the dataset for training model--------
 test_size = 0.2
 col_groups = "test_cas"
 
 df_fishchem = db_mortality[["fish", "test_cas"]]
-trainvalid_idx, valid_idx = get_grouped_train_test_split(
+traintest_idx, valid_idx = get_grouped_train_test_split(
     df_fishchem, test_size, col_groups
 )
-df_fishchem_tv = df_fishchem.iloc[trainvalid_idx, :].reset_index(drop=True)
+df_fishchem_tv = df_fishchem.iloc[traintest_idx, :].reset_index(drop=True)
 
-X_trainvalid = (
+X_traintest = (
     db_mortality.drop(columns="conc1_mean")
-    .iloc[trainvalid_idx, :]
+    .iloc[traintest_idx, :]
     .reset_index(drop=True)
 )
 X_valid = (
     db_mortality.drop(columns="conc1_mean").iloc[valid_idx, :].reset_index(drop=True)
 )
-Y_trainvalid = db_mortality.iloc[trainvalid_idx, :].conc1_mean
+Y_traintest = db_mortality.iloc[traintest_idx, :].conc1_mean
 Y_valid = db_mortality.iloc[valid_idx, :].conc1_mean
 
-if "conc1_mean" in list(X_trainvalid.columns):
-    print("yes")
-else:
-    print("no")
 
+# -----------------creating the distance matrix--------
 
 print("calcultaing distance matrix..", ctime())
-matrix_h, matrix_p = cal_matrixs(
-    X_trainvalid, X_trainvalid, categorical, non_categorical
-)
+matrices = cal_matrixs(X_traintest, X_traintest, categorical, non_categorical)
 print("distance matrix calculation finished", ctime())
 
-
+# -----------------train model--------
 hyper_params_tune = {
     "max_depth": [i for i in range(10, 30, 6)],
     "n_estimators": [int(x) for x in np.linspace(start=200, stop=1000, num=11)],
@@ -96,17 +106,19 @@ hyper_params_tune = {
     "min_samples_leaf": [1, 2, 4, 8, 16, 32],
 }
 
-params_comb = list(ParameterSampler(hyper_params_tune, n_iter=100, random_state=rand))
+params_comb = list(
+    ParameterSampler(hyper_params_tune, n_iter=args.niter, random_state=rand)
+)
 
 best_accs = 0
-best_p = dict()
-
+best_param = dict()
 if args.alpha_h == "logspace":
-    sequence_ap = np.logspace(-2, 0, 20)
+    sequence_ap = np.logspace(-2, 0, 2)
     sequence_ah = sequence_ap
 else:
     sequence_ap = [float(args.alpha_p)]
     sequence_ah = [float(args.alpha_h)]
+
 count = 1
 for ah in sequence_ah:
     for ap in sequence_ap:
@@ -118,21 +130,25 @@ for ah in sequence_ah:
                 end="\r",
             )
             count = count + 1
-            model = RandomForestClassifier()
-            # model = LogisticRegression(random_state=10, n_jobs=60)
+            if args.model == "rf":
+                model = RandomForestClassifier(random_state=10)
+            elif args.model == "lr":
+                model = LogisticRegression(random_state=10)
+
             for k, v in params_comb[i].items():
                 setattr(model, k, v)
 
-            best_results = RASAR_simple(
+            results = RASAR_simple(
                 df_fishchem_tv,
                 col_groups,
-                matrix_h,
-                matrix_p,
+                matrices["euc"],
+                matrices["hamming"],
+                matrices["pubchem"],
                 ah,
                 ap,
-                X_trainvalid,
-                Y_trainvalid,
-                db_invitro_matrix="noinvitro",
+                X_traintest,
+                Y_traintest.values,
+                db_invitro_matrices="noinvitro",
                 n_neighbors=args.n_neighbors,
                 invitro=args.w_invitro,
                 invitro_form=args.invitro_label,
@@ -141,31 +157,48 @@ for ah in sequence_ah:
                 model=model,
             )
 
-            if best_results["avg_accs"] > best_accs:
-                best_p = params_comb[i]
-                best_accs = best_results["avg_accs"]
-                best_results = best_results
-                best_ah = ah
-                best_ap = ap
+            if results["accuracy"].mean() > best_accs:
+
+                best_accs = results["accuracy"].mean()
+
+                results.loc[0, "ah"] = ah
+                results.loc[0, "ap"] = ap
+                for k, v in best_param.items():
+                    results.loc[0, k] = v
+
+                best_results = results
+
+                best_param = params_comb[i]
+
 
 # -------------------tested on test dataset--------------------
-for k, v in best_p.items():
+for k, v in best_param.items():
     setattr(model, k, v)
 
 
-minmax = MinMaxScaler().fit(X_trainvalid[non_categorical])
-X_trainvalid[non_categorical] = minmax.transform(X_trainvalid.loc[:, non_categorical])
+minmax = MinMaxScaler().fit(X_traintest[non_categorical])
+X_traintest[non_categorical] = minmax.transform(X_traintest.loc[:, non_categorical])
 X_valid[non_categorical] = minmax.transform(X_valid.loc[:, non_categorical])
 
-matrix_test = dist_matrix(
-    X_valid, X_trainvalid, non_categorical, categorical, best_ah, best_ap
+matrix_valid = dist_matrix(
+    X_valid,
+    X_traintest,
+    non_categorical,
+    categorical,
+    best_results.iloc[0]["ah"],
+    best_results.iloc[0]["ap"],
 )
-matrix_train = dist_matrix(
-    X_trainvalid, X_trainvalid, non_categorical, categorical, best_ah, best_ap
+matrix_traintest = dist_matrix(
+    X_traintest,
+    X_traintest,
+    non_categorical,
+    categorical,
+    best_results.iloc[0]["ah"],
+    best_results.iloc[0]["ap"],
 )
 
-train_rf, test_rf = cal_s_rasar(
-    matrix_train, matrix_test, Y_trainvalid, args.n_neighbors, encoding
+traintest_rf, valid_rf = cal_s_rasar(
+    matrix_traintest, matrix_valid, Y_traintest.values, args.n_neighbors, encoding
 )
 invitro_form = args.invitro_label
 db_invitro = args.db_invitro
@@ -173,143 +206,95 @@ invitro = args.w_invitro
 
 
 if invitro == "own":
-    train_rf = pd.DataFrame()
-    test_rf = pd.DataFrame()
+    traintest_rf = pd.DataFrame()
+    valid_rf = pd.DataFrame()
 
-if str(db_invitro) == "overlap":
-    if (invitro != "False") & (invitro_form == "number"):
-        train_rf["invitro_conc"] = X_trainvalid.invitro_conc.reset_index(drop=True)
-        test_rf["invitro_conc"] = X_valid.invitro_conc.reset_index(drop=True)
-    elif (invitro != "False") & (invitro_form == "label"):
-        train_rf["invitro_label"] = X_trainvalid.invitro_label.reset_index(drop=True)
-        test_rf["invitro_label"] = X_valid.invitro_label.reset_index(drop=True)
-
-    elif (invitro != "False") & (invitro_form == "both"):
-        train_rf["ec50"] = X_trainvalid.invitro_conc.reset_index(drop=True)
-        test_rf["ec50"] = X_valid.invitro_conc.reset_index(drop=True)
-        train_rf["invitro_label"] = X_trainvalid.invitro_label.reset_index(drop=True)
-        test_rf["invitro_label"] = X_valid.invitro_label.reset_index(drop=True)
-else:
-    train_index = X_trainvalid.index
-    test_index = X_valid.index
-    if (invitro != "False") & (invitro_form == "number"):
-        dist = np.array(db_invitro_matrix.iloc[train_index, :].min(axis=1))
-        ls = np.array(db_invitro_matrix.iloc[train_index, :].idxmin(axis=1))
-        conc = db_invitro.iloc[ls, :].invitro_conc.reset_index(drop=True)
-        dist = db_invitro_matrix.lookup(pd.Series(ls).index, pd.Series(ls).values)
-        train_rf["invitro_conc"] = np.array(conc)
-        train_rf["invitro_dist"] = dist
-
-        dist = np.array(db_invitro_matrix.iloc[test_index, :].min(axis=1))
-        ls = np.array(db_invitro_matrix.iloc[test_index, :].idxmin(axis=1))
-        conc = db_invitro.iloc[ls, :].invitro_conc.reset_index(drop=True)
-        dist = db_invitro_matrix.lookup(pd.Series(ls).index, pd.Series(ls).values)
-        test_rf["invitro_conc"] = np.array(conc)
-        test_rf["invitro_dist"] = dist
-
-    elif (invitro != "False") & (invitro_form == "label"):
-        dist = np.array(db_invitro_matrix.iloc[train_index, :].min(axis=1))
-        ls = np.array(db_invitro_matrix.iloc[train_index, :].idxmin(axis=1))
-        label = db_invitro.iloc[ls, :].invitro_label.reset_index(drop=True)
-        dist = db_invitro_matrix.lookup(pd.Series(ls).index, pd.Series(ls).values)
-        train_rf["invitro_label"] = np.array(label)
-        train_rf["invitro_dist"] = dist
-
-        dist = np.array(db_invitro_matrix.iloc[test_index, :].min(axis=1))
-        ls = np.array(db_invitro_matrix.iloc[test_index, :].idxmin(axis=1))
-        label = db_invitro.iloc[ls, :].invitro_label.reset_index(drop=True)
-        dist = db_invitro_matrix.lookup(pd.Series(ls).index, pd.Series(ls).values)
-        test_rf["invitro_label"] = np.array(label)
-        test_rf["invitro_dist"] = dist
-
-    elif (invitro != "False") & (invitro_form == "both"):
-
-        dist = np.array(db_invitro_matrix.iloc[train_index, :].min(axis=1))
-        ls = np.array(db_invitro_matrix.iloc[train_index, :].idxmin(axis=1))
-        conc = db_invitro.iloc[ls, :].invitro_conc.reset_index(drop=True)
-        label = db_invitro.iloc[ls, :].invitro_label.reset_index(drop=True)
-        train_rf["invitro_conc"] = np.array(conc)
-        train_rf["invitro_label"] = np.array(label)
-        train_rf["invitro_dist"] = dist
-
-        dist = np.array(db_invitro_matrix.iloc[test_index, :].min(axis=1))
-        ls = np.array(db_invitro_matrix.iloc[test_index, :].idxmin(axis=1))
-        conc = db_invitro.iloc[ls, :].invitro_conc.reset_index(drop=True)
-        label = db_invitro.iloc[ls, :].invitro_label.reset_index(drop=True)
-        test_rf["invitro_conc"] = np.array(conc)
-        test_rf["invitro_label"] = np.array(label)
-        test_rf["invitro_dist"] = dist
+if invitro != "False" and str(db_invitro) == "overlap":
+    traintest_rf = get_vitroinfo(traintest_rf, X_traintest, traintest_idx, invitro_form)
+    valid_rf = get_vitroinfo(valid_rf, X_valid, valid_idx, invitro_form)
 
 
-model.fit(train_rf, Y_trainvalid)
-y_pred = model.predict(test_rf)
-print(train_rf.columns)
-if encoding == "binary":
-
-    accs = accuracy_score(Y_valid, y_pred)
-    sens = recall_score(Y_valid, y_pred)
-    tn, fp, fn, tp = confusion_matrix(Y_valid, y_pred, labels=[0, 1]).ravel()
-    specs = tn / (tn + fp)
-    precs = precision_score(Y_valid, y_pred)
-    f1 = f1_score(Y_valid, y_pred)
-
-elif encoding == "multiclass":
-    accs = accuracy_score(Y_valid, y_pred)
-    sens = recall_score(Y_valid, y_pred, average="macro")
-    specs = np.nan
-    precs = precision_score(Y_valid, y_pred, average="macro")
-    f1 = f1_score(Y_valid, y_pred, average="macro")
-
-
-print(
-    """Accuracy:  {}, Se.Accuracy:  {} 
-		\nSensitivity:  {}, Se.Sensitivity: {}
-        \nSpecificity:  {}, Se.Specificity:{}
-		\nPrecision:  {}, Se.Precision: {}
-		\nf1_score:{}, Se.f1_score:{}""".format(
-        accs,
-        best_results["se_accs"],
-        sens,
-        best_results["se_sens"],
-        specs,
-        best_results["se_specs"],
-        precs,
-        best_results["se_precs"],
-        f1,
-        best_results["se_f1"],
-    )
+# --------saving the information into a file
+df_output = fit_and_predict(
+    model, traintest_rf, Y_traintest, valid_rf, Y_valid, encoding
 )
+df_output.loc[0, ["ah", "ap"]] = best_results.iloc[0][["ah", "ap"]]
+for k, v in best_param.items():
+    df_output.loc[0, k] = v
+df_output.loc[0, "encoding"] = args.encoding
 
-info = []
-info.append(
-    """Accuracy:  {}, Se.Accuracy:  {} 
-		\nSensitivity:  {}, Se.Sensitivity: {}
-        \nSpecificity:  {}, Se.Specificity:{}
-		\nPrecision:  {}, Se.Precision: {}
-		\nf1_score:{}, Se.f1_score:{}""".format(
-        accs,
-        best_results["se_accs"],
-        sens,
-        best_results["se_sens"],
-        specs,
-        best_results["se_specs"],
-        precs,
-        best_results["se_precs"],
-        f1,
-        best_results["se_f1"],
-    )
-)
-info.append("Alpha_h:{}, Alpha_p: {}".format(args.alpha_h, args.alpha_p))
-info.append("Random state:{}".format(rand))
+df2file(df_output, args.outputFile + ".txt")
 
-filename = args.outputFile
-dirname = os.path.dirname(filename)
-if not os.path.exists(dirname):
-    os.makedirs(dirname)
 
-with open(filename, "w") as file_handler:
-    for item in info:
-        file_handler.write("{}\n".format(item))
+# model.fit(traintest_rf, Y_traintest)
+# y_pred = model.predict(valid_rf)
+# print(traintest_rf.columns)
+
+# if encoding == "binary":
+#     accs = accuracy_score(Y_valid, y_pred)
+#     sens = recall_score(Y_valid, y_pred)
+#     tn, fp, fn, tp = confusion_matrix(Y_valid, y_pred, labels=[0, 1]).ravel()
+#     specs = tn / (tn + fp)
+#     precs = precision_score(Y_valid, y_pred)
+#     f1 = f1_score(Y_valid, y_pred)
+
+# elif encoding == "multiclass":
+#     accs = accuracy_score(Y_valid, y_pred)
+#     sens = recall_score(Y_valid, y_pred, average="macro")
+#     specs = np.nan
+#     precs = precision_score(Y_valid, y_pred, average="macro")
+#     f1 = f1_score(Y_valid, y_pred, average="macro")
+
+
+# print(
+#     """Accuracy:  {}, Se.Accuracy:  {}
+# 		\nSensitivity:  {}, Se.Sensitivity: {}
+#         \nSpecificity:  {}, Se.Specificity:{}
+# 		\nPrecision:  {}, Se.Precision: {}
+# 		\nf1_score:{}, Se.f1_score:{}""".format(
+#         accs,
+#         best_results["se_accs"],
+#         sens,
+#         best_results["se_sens"],
+#         specs,
+#         best_results["se_specs"],
+#         precs,
+#         best_results["se_precs"],
+#         f1,
+#         best_results["se_f1"],
+#     )
+# )
+
+# info = []
+# info.append(
+#     """Accuracy:  {}, Se.Accuracy:  {}
+# 		\nSensitivity:  {}, Se.Sensitivity: {}
+#         \nSpecificity:  {}, Se.Specificity:{}
+# 		\nPrecision:  {}, Se.Precision: {}
+# 		\nf1_score:{}, Se.f1_score:{}""".format(
+#         accs,
+#         best_results["se_accs"],
+#         sens,
+#         best_results["se_sens"],
+#         specs,
+#         best_results["se_specs"],
+#         precs,
+#         best_results["se_precs"],
+#         f1,
+#         best_results["se_f1"],
+#     )
+# )
+# info.append("Alpha_h:{}, Alpha_p: {}".format(args.alpha_h, args.alpha_p))
+# info.append("Random state:{}".format(rand))
+
+# filename = args.outputFile
+# dirname = os.path.dirname(filename)
+# if not os.path.exists(dirname):
+#     os.makedirs(dirname)
+
+# with open(filename, "w") as file_handler:
+#     for item in info:
+#         file_handler.write("{}\n".format(item))
 
 
 # python RASAR_simple_cte.py -i1 lc_db_processed.csv -ah 0.0749 -ap 0.5623  -o rasar/s_rf_bi.txt
